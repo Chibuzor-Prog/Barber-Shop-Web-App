117e686f82c6eaee8c351879c5a2ee8215d98c00
Tommy Tran <prof.blobfish@gmail.com>
2026-04-06 17:38:40 -0500

    Fixed estimated time display in /services.

--- queuesmart-app/src/components/admin/ServiceManagement.tsx
+++ queuesmart-app/src/components/admin/ServiceManagement.tsx
@@ -314,7 +314,7 @@ const ServiceManagement: React.FC = () => {
                   {/* Duration */}
                   <div className="col-span-2 text-center">
                     <span className="inline-flex items-center rounded-full bg-blue-50 px-2 py-1 text-xs font-semibold text-blue-700">
-                      {svc.duration} min
+                      {svc.expectedDuration} min
                     </span>
                   </div>
 
